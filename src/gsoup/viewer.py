138dd21c1fb097101136c815057bc1c56e3146cd
Yotam Erel <erelyotam@gmail.com>
2023-01-05 10:54:01 +0900

    minor change to viewer

--- src/gsoup/viewer.py
+++ src/gsoup/viewer.py
@@ -131,6 +131,7 @@ def register_mesh(ps, name, v, f, transparency=1.0, edge_width=0., c_vertices=No
     if v_vertices is not None:
         ps_mesh.add_vector_quantity("vecs", v_vertices,
                                     enabled=True,
+                                    # vectortype='ambient',
                                     radius=0.01,
                                     length=0.1,
                                     color=(0.2, 0.5, 0.5))
